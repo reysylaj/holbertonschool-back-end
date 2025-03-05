62b6c8c585e245bd70a8a06ee4dc323c3cc7366f
reysylaj <rey.sylaj.20@gmail.com>
2025-03-05 22:01:09 +0100

    Task 4

---
+++ python-web_framework/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """Displays 'C' followed by the value of the text variable with underscores replaced by spaces"""
+    new_text = text.replace('_', ' ')
+    return "C " + new_text
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    """Displays 'Python' followed by the value of the text variable with underscores replaced by spaces"""
+    new_text = text.replace('_', ' ')
+    return "Python " + new_text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """Displays 'n is a number' only if n is an integer"""
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
